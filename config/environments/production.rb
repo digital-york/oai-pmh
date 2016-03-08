4f4f407bcaf9280589ea5d66d30b3dbe28bd6a56
Frank <yf508@users.noreply.github.com>
2016-03-08 15:38:45 +0000

    Update production.rb
    changing the value of config.eager_load to 'false' will make dc:title appearing in production mode.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,9 @@ App1::Application.configure do
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  
+  # set config.eager_load to 'true' will make 'dc:title' hidden in production mode
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
